bd9214d11fb3a3a701a91f6c9447f87e1af7ce83
Finn Lobnow <flobnow@gmail.com>
2023-01-15 18:52:40 +0100

    added complex R Prep script

---
+++ scripts/02_R_PREP_complex.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# SCRIPTS FOLDER NAME = $1
+# OUTPUT FILE NAME    = $2
+
+source ./00_source.inc
+
+### ENTER SCRIPTS FOLDER AND MERGE SLURM FILES INTO 'SLURM.OUT'
+cd ${LOC_SCRIPTS}/myRuns/${1}
+cat slurm* > ~/fluffyAlpaca/output_files/${2}/slurm.out
+mkdir -p ${LOC_SCRIPTS}/${1}/temp/
+mv slurm* ${LOC_SCRIPTS}/${1}/temp/
+
+# ENTER THE OUTPUT FOLDER
+cd ~/fluffyAlpaca/output_files/$2
+
+# RENAME FILES (PKL FILES ARE MOVED TO A JOINED PKL FOLDER)
+for i in {1..5}; do
+  mv model_${i}_*_*_*_*.pdb ${2}_model_${i}.pdb
+  [ -f model_${i}_*_*_*_*.pkl ]; rm model_${i}_*_*_*_*.pkl
+  mv relaxed_model_${i}_*   ${2}_rlx_model_${i}.pdb
+  mv ranking_model_${i}_*   ${2}_ranking_model_${i}.json
+done
+
+[ -f checkpoint ]; rm -r checkpoint
